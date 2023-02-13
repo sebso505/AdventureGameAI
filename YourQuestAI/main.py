2dd7f964ecfe2a1abf8eeb3c72714e50209c49f0
Radwoo <radu0895@gmail.com>
2023-02-13 21:35:08 +0200

    Sebi = seful meu

--- YourQuestAI/main.py
+++ YourQuestAI/main.py
@@ -1,15 +1,22 @@
 
-import flask
+
+from flask import render_template, Flask
+# Create the application.
+APP = Flask(__name__)
+
 
 @APP.route('/')
 def index():
     """ Displays the index page accessible at '/'
     """
-    return flask.render_template('index.html', name='Sebi')
+    return render_template('index.html', name='Radu')
 
 
 
 if __name__ == '__main__':
     APP.debug=True
     APP.run()
+    APP.run(host='0.0.0.0')
+    #changes 21:49
 
+    #Changes 6:39
