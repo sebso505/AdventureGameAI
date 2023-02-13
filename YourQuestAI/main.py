250bc402e3f6354ce12a512372b25a7ec764165e
Radwoo <radu0895@gmail.com>
2023-02-13 21:56:49 +0200

    Updated Radu

--- YourQuestAI/main.py
+++ YourQuestAI/main.py
@@ -1,6 +1,6 @@
 
 import flask
-
+from flask import request
 
 # Create the application.
 APP = flask.Flask(__name__)
@@ -11,6 +11,11 @@ def index():
     """ Displays the index page accessible at '/'
     """
     return flask.render_template('index.html', name='Sebi')
+@APP.route('/', methods=['POST'])
+def my_form_post():
+    variable = request.form['variable']
+    flask.render_template('index.html', variable)
+
 
 
 
