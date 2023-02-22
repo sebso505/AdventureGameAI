66758a064bab8cdf6e4e3cbc4e9124f128ae071c
sebso505 <sebi.sto98@gmail.com>
2023-02-22 20:38:24 +0200

    new update Miercuri
    Dall e implement

--- YourQuestAI/main.py
+++ YourQuestAI/main.py
@@ -13,6 +13,7 @@ promptStory = ''
 currentSummary = ''
 currentChoice = ''
 currentStep = 0
+bad_choice = ''
 game_on = True
 
 
@@ -27,6 +28,8 @@ class talkingRobot:
         self.currentChoice = currentChoice
         self.currentStep = currentStep
         self.prompt = ''
+        self.image = ''
+        self.visual = ''
 
     def update_params(self, currentSummary, currentChoice, currentStep):
         self.currentSummary = currentSummary
@@ -34,17 +37,16 @@ class talkingRobot:
         self.currentStep = currentStep
 
     def plot_of_game(self, ):
-        if currentStep == 0:
+        if not game_on:
+            self.prompt = f"{self.robot_act}Based on {self.currentSummary} and the fact that this choice was picked {self.currentChoice} create an ending for the story, where {self.name} is defeated tragically, 2 or 3 sentences long."
+        elif currentStep == 0:
             self.prompt = f"{self.robot_act}Based on the book: {self.book} create an intro, 5 sentences long, in which you introduce me, {self.name}, the main character , in the game. You narrate this intro about me."
         elif currentStep == 8:
             self.prompt = f"{self.robot_act}Based on {self.currentSummary} and the fact that this choice was picked {self.currentChoice} create a story that is nearing its conclusion, 2 or 3 sentences long."
         elif currentStep == 9:
-            self.prompt = f"{self.robot_act}Based on {self.currentSummary} and the fact that this choice was picked {self.currentChoice} create an ending for the story, 2 or 3 sentences long."
+            self.prompt = f"{self.robot_act}Based on {self.currentSummary} and the fact that this choice was picked {self.currentChoice} create a cliffhanger for the story, 2 or 3 sentences long."
         elif currentStep == 10:
-            if game_on:
-                self.prompt = f"{self.robot_act}Based on {self.currentSummary} and the fact that this choice was picked {self.currentChoice} create an ending for the story, where {self.name} emerges victorious, 2 or 3 sentences long."
-            else:
-                self.prompt = f"{self.robot_act}Based on {self.currentSummary} and the fact that this choice was picked {self.currentChoice} create an ending for the story, where {self.name} is defeated tragically, 2 or 3 sentences long."
+            self.prompt = f"{self.robot_act}Based on {self.currentSummary} and the fact that this choice was picked {self.currentChoice} create an ending for the story, where {self.name} emerges victorious, 2 or 3 sentences long."
         else:
             self.prompt = f"{self.robot_act}Based on {self.currentSummary} and the fact that this choice was picked {self.currentChoice} create a plot, 2 or 3 sentences long"
         self.plot = openai.Completion.create(
@@ -74,7 +76,7 @@ class talkingRobot:
     def choices_robot(self):
         self.choices = openai.Completion.create(
             engine="text-davinci-003",
-            prompt=f"{self.robot_act} Reading this plot: {self.plot} generate 3 choices, each one on a new line of text, noted 1. through 3. from which I should choose to continue the game. It is a must that you make one of the choices bad. Also, at the end of the bad one, write the character ~, but for the other choices, dont write anything after them.",
+            prompt=f"{self.robot_act} Reading this plot: {self.plot} generate 3 choices, noted 1. through 3. from which I should choose to continue the game. It is a must that you make one of the choices bad. Also, at the end of the bad one, write the character ~, but for the other choices, dont write anything after them.",
             max_tokens=300,
             n=1,
             stop=None,
@@ -88,10 +90,25 @@ class talkingRobot:
         self.options = self.choices.split("\n")
         self.final_choice = [self.options[2], self.options[3], self.options[4]]
         return self.final_choice
-
-
-
-
+    
+    def createVisualDesc(self):
+        self.visual = openai.Completion.create(
+            engine="text-davinci-003",
+            prompt=f"Reading this plot: {self.summary} generate a visual description, 2 sentences long. Keep in mind that i will use this to generate an image with DALL-E",
+            max_tokens=300,
+            n=1,
+            stop=None,
+            temperature=0.8,
+        ).get("choices")[0].text
+        print(f"The visual desc is: {self.visual}")
+        return self.visual
+    def generateImage(self):
+        self.image = openai.Image.create(
+        prompt=f"A gorgeous  digital illustration of {self.visual}, detailed, trending in artstation, fantasy, mesmerizing, captivating",
+        n=2,
+        size="512x512"
+        ).get("data")[0].url
+        return self.image
 @APP.route('/')
 def index():
     """ Displays the index page accessible at '/'
@@ -99,27 +116,34 @@ def index():
     return flask.render_template('introPage.html', name='Sebi')
 
 @APP.route('/', methods=['POST'])
-def add1():
+def add():
     global promptName
     global promptStory
     global currentSummary
     global currentChoice
     global currentStep
     global game_on
+    global bad_choice
+    print(f'game status {game_on}')
     if request.method == 'POST':
         if currentStep == 0:
             promptName = request.form.get('variableName')
             promptStory = request.form.get('variableStory')
         else:
             currentChoice = request.values.get('choice')
-            print(currentChoice)
+            if currentChoice == bad_choice:
+                game_on = False
+                print(f'game status {game_on}')
+                print(currentChoice)
         game = talkingRobot(promptName, promptStory, currentSummary, currentChoice, currentStep)
         if currentStep < 10 and game_on:
             game.update_params(currentSummary, currentChoice, currentStep)
             plot = game.plot_of_game()
             currentSummary = game.summary_of_plot()
+            game.createVisualDesc()
+            imageGen = game.generateImage()
+            print(imageGen)
             game.choices_robot()
-            bad_choice = ''
             choices = game.choices_separated()
             print(f'Alegeri in if {choices}')
             for i, choice in enumerate(choices):
@@ -127,37 +151,17 @@ def add1():
                     choices[i] = choice.replace('~', '')
                     bad_choice = choices[i]
             currentStep += 1
-            print(currentStep)
-            return flask.render_template('GameTab.html', plot=plot, choice1=choices[0], choice2=choices[1], choice3=choices[2], bad_choice=bad_choice)
-
-
-@APP.route('/GameTab', methods=['POST'])
-def add2():
-    global promptName
-    global promptStory
-    global currentSummary
-    global currentChoice
-    global currentStep
-    global game_on
-    if request.method == 'POST':
-        if game_on:
-            while currentStep < 10 and game_on:
-                game.update_params(currentSummary, currentChoice, currentStep)
-                plot = game.plot_of_game()
-                currentSummary = game.summary_of_plot()
-                game.choices_robot()
-                bad_choice = ''
-                choices = game.choices_separated()
-                for i, choice in enumerate(choices):
-                    if '~' in choice:
-                        choices[i] = choice.replace('~', '')
-                        bad_choice = choices[i]
-                if currentChoice == bad_choice or currentStep == 10:
-                    plot = game.plot_of_game()
-                    game_on = False
-                currentStep += 1
-                choices.clear()
-                return flask.render_template('GameTab.html')
+            print(f'Pasul curent {currentStep}')
+            print(f'Alegere Gresita {bad_choice}')
+            return flask.render_template('GameTab.html', plot=plot, choice1=choices[0], choice2=choices[1], choice3 = choices[2], bad_choice=bad_choice, image=imageGen)
+        else:
+            game.update_params(currentSummary, currentChoice, currentStep)
+            plot = game.plot_of_game()
+            currentSummary = game.summary_of_plot()
+            # if game_on:
+            #     return flask.render_template('GoodEnd.html', plot=plot)
+            # else:
+            #     return flask.render_template('BadEnd.html', plot=plot)
 
 
 if __name__ == '__main__':
